7388df40ac205ec7f0973bb4cdf6633ffedcd80c
marcpelve <marc.pelve@gmail.com>
2019-09-11 13:12:30 -0400

    Create edit and delete actions
    - Created update and delete api request.
    - Created update and delete event handler.
    - Created update and delete ui responses.

--- assets/scripts/expenses/api.js
+++ assets/scripts/expenses/api.js
@@ -13,6 +13,16 @@ const indexExpenses = data => {
   })
 }
 
+const showExpense = expenseId => {
+  return $.ajax({
+    url: config.apiUrl + '/expenses/' + expenseId,
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 const newExpense = data => {
   return $.ajax({
     url: config.apiUrl + '/expenses',
@@ -24,7 +34,31 @@ const newExpense = data => {
   })
 }
 
+const updateExpense = (data, expenseId) => {
+  return $.ajax({
+    url: config.apiUrl + '/expenses/' + expenseId,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deleteExpense = expenseId => {
+  return $.ajax({
+    url: config.apiUrl + '/expenses/' + expenseId,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
   indexExpenses,
-  newExpense
+  showExpense,
+  newExpense,
+  updateExpense,
+  deleteExpense
 }
